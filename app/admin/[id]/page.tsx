d87069198b6d4ce3ed64cd99ef508ea0ed5d4ec8
Luanhai3 <hoangthienluan17@gmail.com>
2026-02-02 11:52:22 +0700

    fix bugs

--- app/admin/[id]/page.tsx
+++ app/admin/[id]/page.tsx
@@ -1,19 +1,23 @@
 import { createClient } from "@/lib/supabase/server";
 import AdminForm from "../AdminForm";
+import { notFound } from "next/navigation";
 
-type Props = {
-	params: Promise<{ slug: string }>;
-  };
-  
-  export default async function Page({ params }: Props) {
-	const { slug } = await params;
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+export default async function EditPlacePage({ params }: Props) {
+  const { id } = await params;
+
   const supabase = await createClient();
 
   const { data: place } = await supabase
     .from("places")
     .select("*")
-    .eq("id", slug)
+    .eq("id", id)
     .single();
+
+  if (!place) return notFound();
 
   return <AdminForm place={place} />;
 }
